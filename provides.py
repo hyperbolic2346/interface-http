9524643a98c31855e634df795281063f88d62804
Cory Johns <johnsca@gmail.com>
2017-11-21 11:22:53 -0500

    Convert to endpoints

--- provides.py
+++ provides.py
@@ -1,30 +1,27 @@
 from charmhelpers.core import hookenv
-from charms.reactive import hook
-from charms.reactive import RelationBase
-from charms.reactive import scopes
+from charms.reactive import when, when_not
+from charms.reactive import set_flag, clear_flag
+from charms.reactive import Endpoint
 
 
-class HttpProvides(RelationBase):
-    scope = scopes.GLOBAL
+class HttpProvides(Endpoint):
 
-    @hook('{provides:http}-relation-{joined,changed}')
-    def changed(self):
-        self.set_state('{relation_name}.available')
+    @when('endpoint.{endpoint_name}.joined')
+    def joined(self):
+        set_flag(self.expand_name('{endpoint_name}.available'))
 
-    @hook('{provides:http}-relation-departed')
-    def departed(self):
-        if len(self.conversation().units) == 1:
-            # this is the last departing unit
-            self.remove_state('{relation_name}.available')
+    @when_not('endpoint.{endpoint_name}.joined')
+    def broken(self):
+        clear_flag(self.expand_name('{endpoint_name}.available'))
 
     def configure(self, port, private_address=None, hostname=None):
         if not hostname:
             hostname = hookenv.unit_get('private-address')
         if not private_address:
             private_address = hookenv.unit_get('private-address')
-        relation_info = {
-            'hostname': hostname,
-            'private-address': private_address,
-            'port': port,
-        }
-        self.set_remote(**relation_info)
+        for relation in self.relations:
+            relation.to_publish.update({
+                'hostname': hostname,
+                'private-address': private_address,
+                'port': port,
+            })
